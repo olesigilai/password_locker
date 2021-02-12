a03299658a88470e7afd9678e316661f40b1feab
olesigilai <ole_sigilai@yahoo.com>
2021-02-13 01:09:38 +0300

    class CRedentials is created and initialized

--- user.py
+++ user.py
@@ -7,3 +7,46 @@ class User:
     user_list = []
     def __init__(self,username,password)
         self.username = username
+        self.password = password
+    def save_user(self):
+        '''
+        save_user method saves a new user object to user list
+        '''
+        User.user_list.append(self)
+        
+    def delete_user(self):
+        '''
+        delete_user method deletes a saved user from user_list
+        '''
+        User.user_list.remove(self)
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+class Credentials:
+    credentials_list = []
+    def __init__(self,account,username,password):
+        self.account = account
+        self.password = password
+        self.username = username
+    # @classmethod
+    # def verify_user(cls,username,password):
+    #     a_user = ''
+    #     for user in User.user_list:
+    #         if(user.username == username and user.password == password):
+    #             a_user == user.username
+    #             return a_user
+    # def save_user_credentials(self):
+    #     '''
+    #     save_user_credential method saves a new user object to credentials list
+    #     '''
+    #     Credentials.credentials_list.append(self)
+    # def delete_credentials(self):
+    #     '''
+    #     delete saved credentials in the credentials list
+    #     '''
+    #     Credentials.credentials_list.remove(self)
+    # @classmethod
+    # def find_by_number(cls,account):
+    #     '''
+
+    #     '''

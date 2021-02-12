0b7dccf497a3c80790390ad8c9266122c570e814
olesigilai <ole_sigilai@yahoo.com>
2021-02-13 00:40:04 +0300

    class User is created

---
+++ user.py
@@ -0,0 +1,9 @@
+
+
+class User:
+    '''
+    class that generate instance of user and user_list password
+    '''
+    user_list = []
+    def __init__(self,username,password)
+        self.username = username
